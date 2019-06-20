d5e15e36201773cb671f371baf392cc3873eabdb
ZakiMohammed <shaikhzaki67@gmail.com>
2019-06-20 18:32:48 +0530

    customer

--- src/app/modules/customer/components/customer-list/customer-list.component.ts
+++ src/app/modules/customer/components/customer-list/customer-list.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListComponent implements OnInit {
 
-  constructor() { }
+  dtOptions: DataTables.Settings = {};
 
-  ngOnInit() {
+  constructor() { }
+  
+  ngOnInit(): void {
+    this.dtOptions = {
+      ajax: 'http://172.16.1.161:8081/study/app-admin/api/customers.php',
+      columns: [
+        { title: 'ID', data: 'id' }, 
+        { title: 'First Name', data: 'firstName' }, 
+        { title: 'Last Name', data: 'lastName' },
+        { title: 'Mobile', data: 'mobile' },
+        { title: 'Email', data: 'email' },
+        {
+          render: (data, type, full, meta) => {
+            return '<center><a href="/customers/1" class="btn btn-warning btn-sm">View</a></center>';
+          }
+        }
+
+      ]
+    };
   }
 
 }
